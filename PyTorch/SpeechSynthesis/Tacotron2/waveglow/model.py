bc6c9f708b414e0dedf6ede8416eaae8709a02fb
Soumith Chintala <soumith@gmail.com>
2019-10-31 00:30:21 -0400

    Update model.py

--- PyTorch/SpeechSynthesis/Tacotron2/waveglow/model.py
+++ PyTorch/SpeechSynthesis/Tacotron2/waveglow/model.py
@@ -75,8 +75,8 @@ class Invertible1x1Conv(torch.nn.Module):
                     W_inverse = W_inverse.half()
                 self.W_inverse = W_inverse
             torch.cuda.synchronize()
-            print(z.dtype, z.shape, z.device, z.stride())
-            print(self.W_inverse.dtype, self.W_inverse.shape, self.W_inverse.device, self.W_inverse.stride())            
+            print(z.dtype, z.shape, z.device, z.stride(), z.storage_offset(), z.contiguous())
+            print(self.W_inverse.dtype, self.W_inverse.shape, self.W_inverse.device, self.W_inverse.stride(), self.W_inverse.storage_offset(), self.W_inverse.contiguous())            
             z = F.conv1d(z, self.W_inverse, bias=None, stride=1, padding=0)
             print('here')
             return z
