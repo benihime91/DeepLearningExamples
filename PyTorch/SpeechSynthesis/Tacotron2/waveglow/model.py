e086c444a6128d0dfc9f0fced150f9057a4db3b4
Soumith Chintala <soumith@gmail.com>
2019-10-31 00:01:27 -0400

    Update model.py

--- PyTorch/SpeechSynthesis/Tacotron2/waveglow/model.py
+++ PyTorch/SpeechSynthesis/Tacotron2/waveglow/model.py
@@ -74,6 +74,8 @@ class Invertible1x1Conv(torch.nn.Module):
                 if z.type() == 'torch.cuda.HalfTensor' or z.type() == 'torch.HalfTensor':
                     W_inverse = W_inverse.half()
                 self.W_inverse = W_inverse
+            print(z.dtype, z.shape)
+            print(W_inverse.dtype, W_inverse.shape)            
             z = F.conv1d(z, self.W_inverse, bias=None, stride=1, padding=0)
             return z
         else:
